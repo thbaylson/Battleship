305d4f0f7b2316bd7632a5e4d84fd525c911ae75
thbaylson <thbaylson@gmail.com>
2020-11-03 16:51:10 -0500

    Added lab 6 files

---
+++ Lab6/AnotherAdderWorker.java
@@ -0,0 +1,23 @@
+/**
+ * Scott Barlowe
+ * Additional AdderWorker for thread exercise.
+ *
+ *
+ */
+
+
+public class AnotherAdderWorker implements Runnable{
+
+    private int sum;
+    private int ID;
+     
+    public AnotherAdderWorker(int id){
+        this.ID = id;
+        sum = 0;
+    }
+
+    public void run(){
+ 
+        
+   }
+}
---
+++ Lab6/ThreadInterrupted.java
@@ -0,0 +1,17 @@
+/**
+ * Scott Barlowe
+ * Mostly empty file for thread exercise
+ *
+ *
+ */
+
+import java.util.*;
+
+public class ThreadInterrupted{
+
+    public static void main(String[] args){
+
+        
+
+    }            
+}
