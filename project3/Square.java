4e49b463b145940bb0fbdf111de6758d648b6ca1
Dillon Gorlesky <dmgorlesky1@catamount.wcu.edu>
2020-11-03 17:41:19 -0500

    Drawboard and Ship Enum

---
+++ project3/Square.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Square {
+
+    //Symbol to draw on each square
+    private String toDraw;
+
+    //If square holds piece
+    public boolean holdsPiece;
+
+    //Add one for length of piece maybe?
+
+    public Square(){
+        this.holdsPiece = false;
+        this.toDraw = "S";
+    }
+
+    //Clears square of ship
+    public void clear(){
+        this.holdsPiece = false;
+        this.toDraw = " ";
+    }
+
+    public void setHoldsPiece(boolean b){
+        this.holdsPiece = b;
+    }
+
+    public boolean getsHoldsPiece(){
+        return this.holdsPiece;
+    }
+
+    public void setToDraw(String symbol){
+        this.toDraw = symbol;
+    }
+
+    public String getToDraw(){
+        return this.toDraw;
+    }
+}
