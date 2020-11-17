a7ce1d327d889155bfa41c24fa44d10e38d4bc3d
Dillon Gorlesky <dmgorlesky1@catamount.wcu.edu>
2020-11-17 17:27:50 -0500

    Client Shiz

--- project3/client/BattleDriver.java
+++ project3/client/BattleDriver.java
@@ -27,24 +27,24 @@ public class BattleDriver {
      */
     public static void main(String[] args) throws UnknownHostException, 
         NumberFormatException {
-        /**if(args.length < 3){
-            System.out.println("Usage: java client.BattleDriver hostname 
-            port nickname");
+        if(args.length < 3){
+            System.out.println("Usage: java client.BattleDriver hostname " +
+            "port nickname");
             System.exit(1);
-        }*/
-        if(args.length < 2){
+        }
+        /**if(args.length < 2){
             System.out.println("Usage: java client.BattleDriver hostname "+
                 "port size");
             //Change size back to nickname
             System.exit(1);
-        }
+        }*/
         try{
             String hostname = args[0];
             int port = Integer.parseInt(args[1]);
-            //String userName = args[2];
+            String userName = args[2];
             
-            BattleClient client = new BattleClient(hostname, port);
-            //BattleClient client = new BattleClient(hostname, port, userName);
+            //BattleClient client = new BattleClient(hostname, port);
+            BattleClient client = new BattleClient(hostname, port, userName);
             client.connect();
         } catch(NumberFormatException e){
             System.out.println("Invalid Port Given. Please Retry.");
