ef1b16c511378248bb729ee23ff1de313a23455d
Dillon Gorlesky <dmgorlesky1@catamount.wcu.edu>
2020-11-20 16:59:56 -0500

    Client Side Input

--- project3/client/BattleClient.java
+++ project3/client/BattleClient.java
@@ -29,13 +29,16 @@ public class BattleClient implements MessageListener{
 
     private InetAddress host;
     private int port;
+    private String echo;
+    private Scanner s;
+    private Socket socket;
     private PrintStreamMessageListener printStream = new PrintStreamMessageListener(System.out);
     private String username;
-    //private Game game;//Current game
     private ConnectionAgent connection;
     private final String invalidCmd1 = "Valid Command are: "+
-                                "\n\t/play \n\t /attack <target> " +
-                                "<[row]> <[col]> \n\t /quit\n\t /show <target>\n";
+                                "\n\t /join <name>\n\t /play \n\t "+ 
+                                "/attack <target> <[row]> <[col]>" +
+                                "\n\t /quit\n\t /show <target>\n";
 
     /** 
      * This constructor is to initialize global variables passed from client
@@ -60,24 +63,32 @@ public class BattleClient implements MessageListener{
      * runs commands.
      */
     public void connect(){
-        Scanner s = new Scanner(System.in);
-        //Make new thread
+        this.s = new Scanner(System.in);
+        //Make new thread???????????????????????????????????????
         String command = "";
         try{              
-            Socket socket = new Socket(this.host, this.port);
+            this.socket = new Socket(this.host, this.port);
             //Making a connection agent 
             this.connection = new ConnectionAgent(socket);
-            //join message
+            //SEND A join message HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            this.echo = "/join " + this.username;
+            send("/join " + this.username);
             while(!command.toLowerCase().equals("/quit")){
                 command = s.nextLine();
                 //SEND COMMAND TO SERVER
                 String[] cmd = command.split(" ");
-                if(cmd.length > 0){
+                if(validLength(cmd)){
+                    //What happens if they hit enter and cmd[0] is null
+                    this.echo = command;
                     send(command);
+                } else {
+                    System.out.println("Not enough arguments given.");
+                    //NOTE FOR LATER
+                    //THE WAY I HAVE IT SET UP IT CHECKS ARGUMENT LENGTHS BUT NOT VALID COMMANDS
+                    System.out.println(invalidCmd1);
                 }
                 //Players can send commands to show boards and such while it isnt their 
                 //turn but the server will check otehr commands such as attack to make sure its their turn
-                //I can make sure the things sent are valid command lengths
             }
             //Send msg to other clients user quit
             //Close socket with sourceClosed()
@@ -98,6 +109,39 @@ public class BattleClient implements MessageListener{
         }
     }
 
+    public boolean validLength(String[] cmd){
+        if(cmd[0].toLowerCase().equals("/attack")){
+            if(cmd.length == 4){
+                return true;
+            }
+        } else if(cmd[0].toLowerCase().equals("/play")){
+            if(cmd.length == 1){
+                return true;
+            }
+        } else if(cmd[0].toLowerCase().equals("/quit")){
+            if(cmd.length == 1){
+                this.username = "";
+                return true;
+            }
+        } else if(cmd[0].toLowerCase().equals("/show")){
+            if(cmd.length == 2){
+                return true;
+            }
+        } else if(cmd[0].toLowerCase().equals("/join")){
+            if(this.username.equals("")){
+                //They can't join a second time
+                if(cmd.length == 2){
+                    return true;
+                }
+            } else {
+                System.out.println("Error: Cannot join a game in which a player"+
+                    " is already playing.");
+            }
+        }
+        //Returns true if its an invalid command and server sends back useage message
+        return true;
+    }
+
     /**
      * Used to notify observers that the subject has received a message.
      *
@@ -106,7 +150,9 @@ public class BattleClient implements MessageListener{
      */
     public void messageReceived(String msg, MessageSource source){
         //There will be echo (same message sent to ConnectionAgent) received and printed again
-        printStream.messageReceived(msg, source);
+        if(!this.echo.equals(msg)){
+            printStream.messageReceived(msg, source);
+        }
     }
 
     /**
@@ -132,6 +178,7 @@ public class BattleClient implements MessageListener{
         //know via broadcast msg I have surrendered
         
         //Do all cleanup
+        this.s.close();
         printStream.sourceClosed(source);
     }
 }
